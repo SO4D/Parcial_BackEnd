04cc00ac7ec9ec238016e04423a121cbca0a8d6f
Gaspar <135924893+SO4D@users.noreply.github.com>
2023-10-11 21:52:23 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# Parcial_BackEnd
-Api Rest con paginación
+# apiRestPag
+
+Persona Api Request.postman_collection --> importar desde Postman para poder utilizar la coleccion
+o
+PruebasconPaginacionApiRequest.postman_collection --> importar desde Postman para poder utilizar la coleccion
+ 
+Cualquiera de los dos archivos funcionan al importarlos hacia al PostMan , la diferencia es que una es tipo .JSON 
+y el otro es una archivo Postman_Collection
+
+LINK RENDER
+
+https://apirestconpagi-serralta47786.onrender.com
---
+++ src/main/java/com/utn/apiRestSinPaginacion/repositories/PersonaRepository.java
@@ -0,0 +1,38 @@
+package com.utn.apiRestSinPaginacion.repositories;
+
+import com.utn.apiRestSinPaginacion.entities.Persona;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+
+@Repository
+public interface PersonaRepository extends BaseRepository<Persona,Long> {
+
+//Anotacion Hetodo de Query
+    List<Persona> findByNombreContainingOrApellidoContaining(String nombre, String apellido);
+    Page<Persona> findByNombreContainingOrApellidoContaining(String nombre, String apellido , Pageable pageable);
+
+//Anotacion JPQL paranetros indexados
+
+    @Query(value = "SELECT p FROM Persona p WHERE p.nombre LIKE %:f1ltro% OR p.apellido LIKE %:filtro%")
+    List<Persona> search(@Param("filtro") String filtro);
+    @Query(value = "SELECT p FROM Persona p WHERE p.nombre LIKE %:f1ltro% OR p.apellido LIKE %:filtro%")
+    Page<Persona> search(@Param("filtro") String filtro , Pageable pageable);
+
+    @Query(value = "SELECT * FROM persona WHERE persona.nombre LIKE %:filtro% OR persona.apellido LIKE %:filtro%",
+            nativeQuery = true)
+    List<Persona> searchNativo(@Param("filtro") String filtro);
+
+    @Query(value = "SELECT * FROM persona WHERE persona.nombre LIKE %:filtro% OR persona.apellido LIKE %:filtro%",
+            countQuery = "SELECT count(*) FROM persona" ,
+            nativeQuery = true)
+    Page<Persona> searchNativo(@Param("filtro") String filtro, Pageable pageable);
+
+
+}
