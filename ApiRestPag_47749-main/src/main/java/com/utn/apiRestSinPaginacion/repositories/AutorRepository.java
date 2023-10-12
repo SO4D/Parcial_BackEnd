1a446f2bf6920d0b8e78e0156187b5ac1278fec2
Gaspar <135924893+SO4D@users.noreply.github.com>
2023-10-11 21:48:31 -0300

    Delete ApiRestPag_47749-main directory

--- ApiRestPag_47749-main/src/main/java/com/utn/apiRestSinPaginacion/repositories/AutorRepository.java
+++
@@ -1,8 +0,0 @@
-package com.utn.apiRestSinPaginacion.repositories;
-
-import com.utn.apiRestSinPaginacion.entities.Autor;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AutorRepository extends BaseRepository<Autor,Long> {
-}
--- ApiRestPag_47749-main/src/main/java/com/utn/apiRestSinPaginacion/services/AutorService.java
+++
@@ -1,6 +0,0 @@
-package com.utn.apiRestSinPaginacion.services;
-
-import com.utn.apiRestSinPaginacion.entities.Autor;
-
-public interface AutorService extends BaseService<Autor, Long> {
-}
